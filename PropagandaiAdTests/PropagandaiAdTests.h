7de1224b6874a90e9cb3f642573217a6998fe727
Rafael Brigagão Paulino <rafa_corre@MacBook-Pro-de-Rafael.local>
2012-10-16 11:08:08 -0300

    Initial Commit

---
+++ PropagandaiAdTests/PropagandaiAdTests.h
@@ -0,0 +1,13 @@
+//
+//  PropagandaiAdTests.h
+//  PropagandaiAdTests
+//
+//  Created by Rafael Brigagão Paulino on 16/10/12.
+//  Copyright (c) 2012 rafapaulino.com. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface PropagandaiAdTests : SenTestCase
+
+@end
---
+++ PropagandaiAdTests/PropagandaiAdTests.m
@@ -0,0 +1,32 @@
+//
+//  PropagandaiAdTests.m
+//  PropagandaiAdTests
+//
+//  Created by Rafael Brigagão Paulino on 16/10/12.
+//  Copyright (c) 2012 rafapaulino.com. All rights reserved.
+//
+
+#import "PropagandaiAdTests.h"
+
+@implementation PropagandaiAdTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in PropagandaiAdTests");
+}
+
+@end
